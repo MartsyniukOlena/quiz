788275c3ebf6714810992e1c158d9cfde471563e
MartsyniukOlena <>
2023-10-18 15:20:34 +0000

    Add questions.js file, and connect it with index.html

---
+++ assets/js/questions.js
@@ -0,0 +1,360 @@
+// Test Questions
+let beginnerLevel = [
+    {
+        question: "What is the opposite of dangerous?",
+        1: "easy",
+        2: "comfortable",
+        3: "safe",
+        answer: "3",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "I have been to the cinema yesterday.",
+        2: "I was at the cinema yesterday.",
+        3: "I was been at the cinema yesterday.",
+        answer: "2",
+    },
+
+    {
+        question: "Which of the following modals doesn't have a short form?",
+        1: "cannot",
+        2: "may not",
+        3: "need not",
+        answer: "2",
+    },
+
+    {
+        question: "What is a busker?",
+        1: "a bus driver",
+        2: "a musical instrument",
+        3: "a street musician",
+        answer: "3",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "You have to learn these words at heart.",
+        2: "You have to learn these words by heart.",
+        3: "You have to learn these words for heart.",
+        answer: "2",
+    },
+
+    {
+        question: "Who's the boss of a tennis match?",
+        1: "leader",
+        2: "manager",
+        3: "umpire",
+        answer: "3",
+    },
+
+    {
+        question: "Which answers to the following question are correct? Whose book is it?",
+        1: "It's mine.",
+        2: "It's my.",
+        3: "It's me.",
+        answer: "1",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "Peter wanted his mother to help him.",
+        2: "Peter wanted his mother to helps him.",
+        3: "Peter wanted that his mother helps him.",
+        answer: "1",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "I didn't get some apples.",
+        2: "I haven't got any time.",
+        3: "There's any milk in the fridge.",
+        answer: "2",
+    },
+
+    {
+        question: "Which question is correct?",
+        1: "Can you give me an advice?",
+        2: "Can you give me some advice?",
+        3: "Can you give me some advices?",
+        answer: "Can you give me some advice?",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "The telephone is calling.",
+        2: "The telephone is calling.",
+        3: "The telephone is ringing.",
+        4: "The telephone is sounding.",
+        answer: "The telephone is ringing.",
+    },
+
+    {
+        question: "What is the opposite of to buy?",
+        1: "to lend",
+        2: "to sell",
+        3: "to take",
+        answer: "to sell",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "He went to school yesterday.",
+        2: "He went yesterday to school.",
+        3: "Yesterday he has gone to school.",
+        answer: "He went to school yesterday.",
+    },
+
+    {
+        question: "What is the answer to this question? I like tennis.",
+        1: "I am also.",
+        2: "So do I.",
+        3: "So like I.",
+        answer: "So do I.",
+    },
+
+    {
+        question: " Which sentence is correct?",
+        1: "My mother works half-time at Burger King.",
+        2: "My mother works part-time at Burger King.",
+        3: "My mother works spare time at Burger King.",
+        answer: "My mother works part-time at Burger King.",
+    },
+];
+
+
+let intermediateLevel = [
+    {
+        question: "Which of the following sentences is written in Passive?",
+        1: "He is reading a book.",
+        2: "I am given a book.",
+        3: "I give him a book.",
+        answer: "I am given a book.",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "I look forward at seeing you soon.",
+        2: "I look forward for seeing you soon.",
+        3: "I look forward to seeing you soon.",
+        answer: "I look forward to seeing you soon.",
+    },
+
+    {
+        question: "Which of the following sentences is written in Passive?",
+        1: "A house is built.",
+        2: "She goes to school.",
+        3: "They read a book.",
+        answer: "A house is built.",
+    },
+
+    {
+        question: "What is the opposite of now?",
+        1: "again",
+        2: "soon",
+        3: "then",
+        answer: "then",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "The policeman made me open the suitcase.",
+        2: "The policeman made me opening the suitcase.",
+        3: "The policeman made me to open the suitcase.",
+        answer: "The policeman made me open the suitcase.",
+    },
+
+    {
+        question: "Complete this phrase: As fresh as",
+        1: "a daisy",
+        2: "milk",
+        3: "Spring",
+        answer: "As fresh as a daisy",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "She is always complaining about her father.",
+        2: "She is always complaining of her father.",
+        3: "She is always complaining to her father",
+        answer: "She is always complaining about her father.",
+    },
+
+    {
+        question: "Which two words sound the same?",
+        1: "feat, feet",
+        2: "fit, feed",
+        3: "fit, feet",
+        answer: "feat, feet",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "Albert Einstein was a great physician.",
+        2: "In my meaning rats are nice animals.",
+        3: "The Yukon is the longest river in Alaska.",
+        answer: "The Yukon is the longest river in Alaska.",
+    },
+
+    {
+        question: "Which is the most polite phrase when telephoning?",
+        1: "Hang on.",
+        2: "Hold on.",
+        3: "Just a moment, please.",
+        answer: "Just a moment, please.",
+    },
+
+    {
+        question: "What do you call a person who you can trust and depend on at all times?",
+        1: "absent-minded",
+        2: "impulsive",
+        3: "reliable",
+        answer: "reliable",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "There isn't a one computer in this office.",
+        2: "There isn't a single computer in this office.",
+        3: "There isn't an only computer in this office.",
+        answer: "There isn't a single computer in this office.",
+    },
+
+    {
+        question: "When you want to buy a house in England, the money you borrow is called:",
+        1: "deposit",
+        2: "loan",
+        3: "mortgage",
+        answer: "mortgage",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: " I'm rather short at money.",
+        2: "I'm rather short of money.",
+        3: "I'm rather short on money.",
+        answer: "I'm rather short of money.",
+    },
+
+    {
+        question: "Which two words sound the same?",
+        1: "well, will",
+        2: "wheel, we'll",
+        3: "will, we'll",
+        answer: "we'll",
+    },
+];
+
+let advancedLevel = [
+    {
+        question: "Which word is closest in meaning to these three: love, like, enjoy?",
+        1: "adore",
+        2: "hope",
+        3: "want",
+        answer: "adore",
+    },
+
+    {
+        question: "Where does a porter work?",
+        1: "in the library",
+        2: "in the supermarket",
+        3: "at the station",
+        answer: "at the station",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "At this time tomorrow I take my driving test.",
+        2: "At this time tomorrow I will be taking my driving test.",
+        3: "At this time tomorrow I will take my driving test.",
+        answer: "At this time tomorrow I will be taking my driving test.",
+    },
+
+    {
+        question: "What do you call a person who was born in Norwich?",
+        1: "a native of Norwich",
+        2: "a resident of Norwich",
+        3: "an inhabitant of Norwich",
+        answer: "a native of Norwich",
+    },
+
+    {
+        question: "What is a synonym to rude?",
+        1: "brave",
+        2: "impolite",
+        3: "stupid",
+        answer: "impolite",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "I want to have a word with you before you go.",
+        2: "I want to have a word with you before you will go.",
+        3: "I want to have a word with you before you will have gone.",
+        answer: "I want to have a word with you before you go.",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "The rent is to be paid at advance.",
+        2: "The rent is to be paid for advance.",
+        3: "The rent is to be paid in advance.",
+        answer: "The rent is to be paid in advance.",
+    },
+
+    {
+        question: "The middle of an apple which we don't eat is called the:",
+        1: "centre",
+        2: "core",
+        3: "heart",
+        answer: "core",
+    },
+
+    {
+        question: "The back end of a ship is called:",
+        1: "the deck",
+        2: "the mast",
+        3: "the stern",
+        answer: "the stern",
+    },
+
+    {
+        question: "Complete this phrase: hale and",
+        1: "fit",
+        2: "healthy",
+        3: "hearty",
+        answer: "hearty",
+    },
+
+    {
+        question: "Which sentence is correct?",
+    1: "This film is at far the best one he's ever made.",
+        2: "This film is by far the best one he's ever made.",
+        3: "This film is so far the best one he's ever made.",
+        answer: "This film is by far the best one he's ever made.",
+    },
+
+    {
+        question: "Which sentence is correct?",
+        1: "I'm going to do my homework.",
+        2: "Do the beds",
+        3: "She often does some coffee.",
+        answer: "I'm going to do my homework.",
+    },
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
